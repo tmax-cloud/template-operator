4aa0989b7bcb126694efeeda7107c186f66c6e57
eddy-kor-92 <16secondlife@gmail.com>
2021-11-29 16:49:58 +0900

    refactor cluster template controller

--- controllers/clustertemplate_controller.go
+++ controllers/clustertemplate_controller.go
@@ -33,7 +33,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
-	tmaxiov1 "github.com/tmax-cloud/template-operator/api/v1"
+	tmplv1 "github.com/tmax-cloud/template-operator/api/v1"
 )
 
 // ClusterTemplateReconciler reconciles a ClusterTemplate object
@@ -51,7 +51,7 @@ func (r *ClusterTemplateReconciler) Reconcile(req ctrl.Request) (ctrl.Result, er
 	reqLogger.Info("Reconciling ClusterTemplate")
 
 	// Fetch the ClusterTemplate instance
-	template := &tmaxiov1.ClusterTemplate{}
+	template := &tmplv1.ClusterTemplate{}
 	err := r.Client.Get(context.TODO(), req.NamespacedName, template)
 	if err != nil {
 		if errors.IsNotFound(err) {
@@ -83,59 +83,42 @@ func (r *ClusterTemplateReconciler) Reconcile(req ctrl.Request) (ctrl.Result, er
 		return ctrl.Result{}, nil
 	}
 
+	// copy reconciling template from original
 	updateInstance := template.DeepCopy()
 
-	setClusterTemplateSpecDefaultField(updateInstance)
+	r.setClusterTemplateSpecDefaultField(updateInstance)
 
-	// add kind to objectKinds fields
-	objectKinds := make([]string, 0)
-	for _, obj := range template.Objects {
-		var in runtime.Object
-		var scope conversion.Scope // While not actually used within the function, need to pass in
-		if err = runtime.Convert_runtime_RawExtension_To_runtime_Object(&obj, &in, scope); err != nil {
-			reqLogger.Error(err, "cannot decode object")
-			templateStatus := &tmaxiov1.TemplateStatus{
-				Message: "cannot decode object",
-				Status:  tmaxiov1.TemplateError,
-			}
-			return r.updateClusterTemplateStatus(template, templateStatus)
-		}
-
-		unstrObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(in)
-		if err != nil {
-			reqLogger.Error(err, "cannot decode object")
-			templateStatus := &tmaxiov1.TemplateStatus{
-				Message: "cannot decode object",
-				Status:  tmaxiov1.TemplateError,
-			}
-			return r.updateClusterTemplateStatus(template, templateStatus)
-		}
-
-		unstr := unstructured.Unstructured{Object: unstrObj}
-		reqLogger.Info(fmt.Sprintf("kind: %s", unstr.GetKind()))
-		objectKinds = append(objectKinds, unstr.GetKind())
-	}
-	updateInstance.ObjectKinds = objectKinds
-	reqLogger.Info(fmt.Sprintf("%v", objectKinds))
-
-	if err = r.Client.Patch(context.TODO(), updateInstance, client.MergeFrom(template)); err != nil {
-		reqLogger.Error(err, "cannot update clustertemplate")
-		templateStatus := &tmaxiov1.TemplateStatus{
-			Message: "cannot update clustertemplate",
-			Status:  tmaxiov1.TemplateError,
+	if err := r.setObjectKinds(updateInstance); err != nil {
+		reqLogger.Error(err, "cannot decode object")
+		templateStatus := &tmplv1.TemplateStatus{
+			Message: "cannot decode object",
+			Status:  tmplv1.TemplateError,
 		}
 		return r.updateClusterTemplateStatus(template, templateStatus)
 	}
 
-	templateStatus := &tmaxiov1.TemplateStatus{
+	reqLogger.Info(fmt.Sprintf("object kinds: %v", updateInstance.ObjectKinds))
+
+	// Patch reconciled template
+	if err = r.Client.Patch(context.TODO(), updateInstance, client.MergeFrom(template)); err != nil {
+		reqLogger.Error(err, "cannot update clustertemplate")
+		templateStatus := &tmplv1.TemplateStatus{
+			Message: "cannot update clustertemplate",
+			Status:  tmplv1.TemplateError,
+		}
+		return r.updateClusterTemplateStatus(template, templateStatus)
+	}
+
+	// update status when succeed
+	templateStatus := &tmplv1.TemplateStatus{
 		Message: "update success",
-		Status:  tmaxiov1.TemplateSuccess,
+		Status:  tmplv1.TemplateSuccess,
 	}
 	return r.updateClusterTemplateStatus(template, templateStatus)
 }
 
-func (r *ClusterTemplateReconciler) updateClaimStatus(reqLogger logr.Logger, ct *tmaxiov1.ClusterTemplate) error {
-	claim := &tmaxiov1.ClusterTemplateClaim{}
+func (r *ClusterTemplateReconciler) updateClaimStatus(reqLogger logr.Logger, ct *tmplv1.ClusterTemplate) error {
+	claim := &tmplv1.ClusterTemplateClaim{}
 
 	claimInfo := strings.Split(ct.ObjectMeta.Labels[claimLabel], ".")
 	claimNamespacedName := types.NamespacedName{
@@ -148,11 +131,11 @@ func (r *ClusterTemplateReconciler) updateClaimStatus(reqLogger logr.Logger, ct 
 	}
 
 	updatedClaim := claim.DeepCopy()
-	updatedClaim.Status = tmaxiov1.ClusterTemplateClaimStatus{
+	updatedClaim.Status = tmplv1.ClusterTemplateClaimStatus{
 		LastTransitionTime: metav1.Time{Time: time.Now()},
 		Reason:             "ClusterTemplate was deleted",
 
-		Status:  tmaxiov1.ClusterTemplateDeleted,
+		Status:  tmplv1.ClusterTemplateDeleted,
 		Handled: true,
 	}
 
@@ -166,7 +149,7 @@ func (r *ClusterTemplateReconciler) updateClaimStatus(reqLogger logr.Logger, ct 
 }
 
 func (r *ClusterTemplateReconciler) updateClusterTemplateStatus(
-	template *tmaxiov1.ClusterTemplate, status *tmaxiov1.TemplateStatus) (ctrl.Result, error) {
+	template *tmplv1.ClusterTemplate, status *tmplv1.TemplateStatus) (ctrl.Result, error) {
 	reqLogger := r.Log.WithName("update clustertemplate status")
 
 	updatedTemplate := template.DeepCopy()
@@ -180,29 +163,51 @@ func (r *ClusterTemplateReconciler) updateClusterTemplateStatus(
 	return ctrl.Result{}, nil
 }
 
-func setClusterTemplateSpecDefaultField(template *tmaxiov1.ClusterTemplate) {
-	if len(template.ShortDescription) == 0 {
+func (r *ClusterTemplateReconciler) setClusterTemplateSpecDefaultField(template *tmplv1.ClusterTemplate) {
+	if template.ShortDescription == "" {
 		template.ShortDescription = template.ObjectMeta.Name
 	}
 
-	if len(template.ImageUrl) == 0 {
+	if template.ImageUrl == "" {
 		template.ImageUrl = "https://folo.co.kr/img/gm_noimage.png"
 	}
-	if len(template.LongDescription) == 0 {
+
+	if template.LongDescription == "" {
 		template.LongDescription = template.ObjectMeta.Name
 	}
 
-	if len(template.MarkDownDescription) == 0 {
+	if template.MarkDownDescription == "" {
 		template.MarkDownDescription = template.ObjectMeta.Name
 	}
 
-	if len(template.Provider) == 0 {
+	if template.Provider == "" {
 		template.Provider = "tmax"
 	}
 }
 
+func (r *ClusterTemplateReconciler) setObjectKinds(template *tmplv1.ClusterTemplate) error {
+	objectKinds := make([]string, 0)
+	for _, obj := range template.Objects {
+		var in runtime.Object
+		var scope conversion.Scope
+		if err := runtime.Convert_runtime_RawExtension_To_runtime_Object(&obj, &in, scope); err != nil {
+			return err
+		}
+
+		if unstrObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(in); err != nil {
+			return err
+		} else {
+			unstr := unstructured.Unstructured{Object: unstrObj}
+			objectKinds = append(objectKinds, unstr.GetKind())
+		}
+	}
+
+	template.ObjectKinds = objectKinds
+	return nil
+}
+
 func (r *ClusterTemplateReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&tmaxiov1.ClusterTemplate{}).
+		For(&tmplv1.ClusterTemplate{}).
 		Complete(r)
 }
